f6cdb6f164eb0d8d1b772d215c7be96f05eba72b
Rah <hackrahul124@gmail.com>
2023-11-24 15:41:19 +0530

    minor fixes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json({ entended: true }))
 app.use(express.urlencoded({ extended: true }));
 app.use("/", Route);
 
-Connection();
+Connection(); 
 
 const PORT = 8000
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
